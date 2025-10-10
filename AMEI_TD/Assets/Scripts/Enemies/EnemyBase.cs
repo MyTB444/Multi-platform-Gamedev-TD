731ddab97639513b14f257c47ad232eca6423b01
Ilja <valasenko30.ilja@gmail.com>
2025-10-10 17:33:39 +0100

    Enemy types, path and moving logic

--- AMEI_TD/Assets/Scripts/Enemies/EnemyBase.cs
+++ AMEI_TD/Assets/Scripts/Enemies/EnemyBase.cs
@@ -2,28 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyType
+{
+    Basic,
+    Fast,
+    Tank,
+}
+
 public class EnemyBase : MonoBehaviour, IDamageable
 {
+    [SerializeField] private EnemyType enemyType;
     [SerializeField] float enemyHp;
     [SerializeField] float enemySpeed;
     [SerializeField] private Transform centerPoint;
+    [SerializeField] private Transform bottomPoint;
+    [SerializeField] private EnemyPath path;
 
+    private int currentWaypointIndex = 0;
+    private float waypointReachDistance = 0.1f;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        if (path != null && path.WaypointCount > 0)
+        {
+            Transform firstWaypoint = path.GetWaypoint(0);
+            if (firstWaypoint != null && bottomPoint != null)
+            {
+                Vector3 offset = transform.position - bottomPoint.position;
+                transform.position = firstWaypoint.position + offset;
+            }
+        }
+        
+        Renderer renderer = GetComponent<Renderer>();
+        switch (enemyType)
+        {
+            case EnemyType.Basic:
+                renderer.material.color = Color.green;
+                break;
+            case EnemyType.Fast:
+                renderer.material.color = Color.magenta;
+                break;
+            case EnemyType.Tank:
+                renderer.material.color = Color.red;
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (enemyHp <= 0)
         {
             Die();
+            return;
+        }
+
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (!path || currentWaypointIndex >= path.WaypointCount)
+        {
+            ReachedEnd();
+            return;
+        }
+
+        Transform targetWaypoint = path.GetWaypoint(currentWaypointIndex);
+        if (!targetWaypoint || !bottomPoint)
+        {
+            return;
+        }
+
+        Vector3 direction = (targetWaypoint.position - bottomPoint.position).normalized;
+    
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+    
+        transform.position += direction * (enemySpeed * Time.deltaTime);
+
+        float distanceToWaypoint = Vector3.Distance(bottomPoint.position, targetWaypoint.position);
+        if (distanceToWaypoint <= waypointReachDistance)
+        {
+            currentWaypointIndex++;
         }
     }
-    
+
+    void ReachedEnd()
+    {
+        Destroy(gameObject);
+    }
 
     // Get Main Damage
     public virtual void TakeDamage(float damage)
@@ -34,6 +106,11 @@ public class EnemyBase : MonoBehaviour, IDamageable
     public Vector3 GetCeterPoint()
     {
         return centerPoint.position;
+    }
+
+    public EnemyType GetEnemyType()
+    {
+        return enemyType;
     }
 
     void Die()
