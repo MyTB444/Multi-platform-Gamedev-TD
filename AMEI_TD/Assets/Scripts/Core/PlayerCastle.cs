983690ccb8f2b04774b04f5599dc3c6f4316f337
adnan <adnanhussain786112@gmail.com>
2025-10-11 22:21:38 +0100

    Player Castle Health

--- AMEI_TD/Assets/Scripts/Core/PlayerCastle.cs
+++ AMEI_TD/Assets/Scripts/Core/PlayerCastle.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCastle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float CastleHealth;
+    [SerializeField] private Slider HealthSlider;
+    private void OnTriggerEnter(Collider other)
     {
-
+        if (other.gameObject != null)
+        {
+            if (other.gameObject.GetComponent<EnemyBase>() != null)
+            {
+                if (CastleHealth <= 100f)
+                {
+                    CastleHealth -= 10f;
+                    HealthSlider.value = CastleHealth;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
 }
