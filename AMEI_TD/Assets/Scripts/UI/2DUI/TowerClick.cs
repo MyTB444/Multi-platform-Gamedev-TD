9d378452a9fa743421e93d6d3d5767460cbcd83e
wkis028 <wkis028@live.rhul.ac.uk>
2025-10-14 11:35:29 +0100

    Added keyboard shortcut for tower deletion

--- AMEI_TD/Assets/Scripts/UI/2DUI/TowerClick.cs
+++ AMEI_TD/Assets/Scripts/UI/2DUI/TowerClick.cs
@@ -3,8 +3,14 @@ using UnityEngine.EventSystems;
 
 public class TowerClick : MonoBehaviour
 {
-
+    private bool onTower = false;
     [SerializeField] private GameObject destroyButton;
+
+    void FixedUpdate()
+    {
+        if (onTower == true)
+            KeyboardDestroy();
+    }
 
     private void OnMouseDown()
     {
@@ -15,6 +21,21 @@ public class TowerClick : MonoBehaviour
         else if (destroyButton.activeSelf == true)
         {
             destroyButton.SetActive(false);
+        }
+    }
+    private void OnMouseEnter()
+    {
+        onTower = true;
+    }
+    private void OnMouseExit()
+    {
+        onTower = false;
+    }
+    private void KeyboardDestroy()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Destroy(this.gameObject);
         }
     }
     public void DestoryTower()
