97d2b0ae83017cdce9844db922fd82a3efc418be
adnan <adnanhussain786112@gmail.com>
2025-11-24 01:28:26 +0000

    Enemy Navemesh Movement With Path Following

--- AMEI_TD/Assets/Scripts/Enemies/Types/EnemyFast.cs
+++ AMEI_TD/Assets/Scripts/Enemies/Types/EnemyFast.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyFast : EnemyBase
 {
+    private NavMeshAgent navAgent;
+    protected int currentWaypointIndex;
+    private void OnEnable()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
 
+    protected override void Update()
+    {
+        FollowPath();
+    }
+    public void FollowPath()
+    {
+        float distanceToWayPoint = Vector3.Distance(myWaypoints[currentWaypointIndex], transform.position);
+
+        if (distanceToWayPoint <= 2)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % myWaypoints.Length;
+        }
+        navAgent.SetDestination(myWaypoints[currentWaypointIndex]);
+    }
 }
