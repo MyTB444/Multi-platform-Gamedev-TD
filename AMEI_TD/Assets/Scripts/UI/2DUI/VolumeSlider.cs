c971ea8b491db42fc63bf80c391a7305a0d32390
Eren Tanriverdi <120788990+MyTB444@users.noreply.github.com>
2026-01-06 13:51:50 +0000

    Added volume slider

---
+++ AMEI_TD/Assets/Scripts/UI/2DUI/VolumeSlider.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        // Load saved volume or default to full
+        float savedVolume = PlayerPrefs.GetFloat("Volume", 0f);
+        volumeSlider.value = savedVolume;
+        SetVolume(savedVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+}
