c5393fb34cd3ac41188da07405fec84d8f8446e2
Eren Tanriverdi <120788990+MyTB444@users.noreply.github.com>
2026-01-06 14:23:59 +0000

    Fixed slider conflict

--- AMEI_TD/Assets/Scripts/UI/2DUI/AudioSLider.cs
+++ AMEI_TD/Assets/Scripts/UI/2DUI/AudioSLider.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class VolumeSlider : MonoBehaviour
+public class AudioSLider : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public Slider volumeSlider;
